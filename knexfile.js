04aa5e1d67c90dfd54d7c3d9dac01298021c30c5
Kim <cid.kim@gmail.com>
2023-05-15 11:41:09 -0600

    knex config connected to database. migrations config working to add migrations to knex>migrations folder

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(
+        __dirname,
+        'movieApi',
+        'src',
+        'database',
+        'database.db'
+      )
+    },
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        'src',
+        'database',
+        'knex',
+        'migrations'
+      )
+    },
+    useNullAsDefault: true
+  }
+};
